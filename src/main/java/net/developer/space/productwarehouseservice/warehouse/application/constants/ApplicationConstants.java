b42968441ee4f048c56569acf7ae7f99fad7a8c1
LazaroDevelop <146339590+LazaroDevelop@users.noreply.github.com>
2025-09-01 13:52:36 -0400

    add application constants

---
+++ src/main/java/net/developer/space/productwarehouseservice/warehouse/application/constants/ApplicationConstants.java
@@ -0,0 +1,5 @@
+package net.developer.space.productwarehouseservice.warehouse.application.constants;
+
+public class ApplicationConstants {
+    public static final String DELETING_MESSAGE = "The User was deleted";
+}
